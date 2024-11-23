1c65db7a8d287599549506625d6c9b3d889de9be
timtoronto634 <tim225x@gmail.com>
2024-11-23 18:32:59 +0900

    create lambda handler (works)

--- lambda_go_handler/main.go
+++ lambda_go_handler/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Order struct {
+	OrderID string  `json:"order_id"`
+	Amount  float64 `json:"amount"`
+	Item    string  `json:"item"`
+}
+
+func handleRequest(ctx context.Context, event json.RawMessage) error {
+	var order Order
+	if err := json.Unmarshal(event, &order); err != nil {
+		log.Printf("Failed to unmarshal event: %v", err)
+		return err
+	}
+
+	log.Printf("Successfully parsed order %+v", order)
+	return nil
+}
 
 func main() {
-	fmt.Println("hello a1")
+	lambda.Start(handleRequest)
 }
