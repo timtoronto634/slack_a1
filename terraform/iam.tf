6f5f2181c95821a0554c6f4378bd9ec112af9d20
timtoronto634 <tim225x@gmail.com>
2024-11-23 17:35:14 +0900

    add terraform directory. create aws iam role

---
+++ terraform/iam.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "bedrock_call_role" {
+  name = "bedrock_call_role"
+
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+
+  tags = {
+    project = "ai-a2"
+  }
+}
+
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "bedrock_call_policy" {
+  name = "bedrock_call_policy"
+  role = aws_iam_role.bedrock_call_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup*",
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:ap-northeast-1:*"
+      },
+      {
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ]
+        Effect   = "Allow"
+        Resource = ["arn:aws:logs:ap-northeast-1:*"]
+      }
+    ]
+  })
+}
---
+++ terraform/version.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.9.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.77.0"
+    }
+  }
+}
